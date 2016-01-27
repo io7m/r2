22486dcb5b8c94a2bb3d5ca4d0f6d4392e920d34
Mark Raynsford <code@io7m.com>
2016-01-27 15:34:14 +0000

    Add diffuse-only spherical lights

---
+++ io7m-r2-shaders/src/main/glsl/com/io7m/r2/shaders/sources/R2LightSphericalDiffuseOnly.h
@@ -0,0 +1,29 @@
+#ifndef R2_LIGHT_SPHERICAL_DIFFUSE_ONLY_H
+#define R2_LIGHT_SPHERICAL_DIFFUSE_ONLY_H
+
+/// \file R2LightSphericalDiffuseOnly.h
+/// \brief A spherical light with no specular highlights
+
+#include "R2LightShaderMain.h"
+#include "R2LightPositional.h"
+#include "R2LightSpherical.h"
+
+uniform R2_light_positional_t R2_light_spherical;
+
+R2_light_output_t
+R2_deferredLightMain(
+  const R2_reconstructed_surface_t surface)
+{
+  R2_light_positional_vectors_t vectors = R2_lightPositionalVectors(
+    R2_light_spherical, surface.position.xyz, surface.normal);
+  float attenuation = R2_lightPositionalAttenuation(
+    R2_light_spherical, vectors.distance);
+
+  vec3 diffuse =
+    R2_lightSphericalDiffuseTerm(R2_light_spherical, vectors);
+
+  return R2_light_output_t(
+    diffuse * attenuation, vec3 (0.0));
+}
+
+#endif // R2_LIGHT_SPHERICAL_DIFFUSE_ONLY_H
---
+++ io7m-r2-shaders/src/main/glsl/com/io7m/r2/shaders/sources/R2LightSphericalDiffuseOnlySingle.frag
@@ -0,0 +1,5 @@
+/// \file R2LightSphericalDiffuseOnlySingle.frag
+/// \brief An instantiation of the `R2LightSphericalDiffuseOnly` shader for single instances
+
+#include "R2LightSphericalDiffuseOnly.h"
+#include "R2LightShaderDriverSingle.h"
