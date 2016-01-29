io7m/r2

commit 7f8866530f2869241f3708797bf211a78c2e888f

Surface shaders now control normal vectors
Surface shaders must now decide which normal vector is used out
of the vertex and mapped normals that they are passed.

Fix #10

io7m-r2-shaders/src/main/glsl/com/io7m/r2/shaders/sources/R2SurfaceBasic.h
@@ -31,6 +31,7 @@ R2_deferredSurfaceMain (
   return R2_surface_output_t (
     surface.xyz,
     emission,
+    derived.normal_eye,
     specular,
     params.specular_exponent
   );

io7m-r2-shaders/src/main/glsl/com/io7m/r2/shaders/sources/R2SurfaceBasicReflective.h
@@ -62,6 +62,7 @@ R2_deferredSurfaceMain (
   return R2_surface_output_t (
     surface.xyz,
     emission,
+    derived.normal_eye,
     specular,
     params.specular_exponent
   );

io7m-r2-shaders/src/main/glsl/com/io7m/r2/shaders/sources/R2SurfaceShaderDriverSingle.h
@@ -55,7 +55,7 @@ R2_surface_shader_main_gbuffer()
   return R2_gbuffer_output_t(
     o.albedo,
     o.emission,
-    normal_comp,
+    o.normal,
     o.specular,
     o.specular_exp / 256.0,
     depth_log

io7m-r2-shaders/src/main/glsl/com/io7m/r2/shaders/sources/R2SurfaceTypes.h
@@ -17,7 +17,7 @@ struct R2_surface_data_t {
   float positive_eye_z;
   /// Object-space UV
   vec2 uv;
-  /// Object-space normal
+  /// Object-space vertex normal
   vec3 normal;
   /// Tangent vector
   vec3 tangent;

io7m-r2-shaders/src/main/glsl/com/io7m/r2/shaders/sources/R2SurfaceOutput.h
@@ -11,6 +11,8 @@ struct R2_surface_output_t {
   vec3  albedo;
   /// 8-bit unsigned normalized emission level
   float emission;
+  /// Eye-space normal vector
+  vec3 normal;
   /// 8-bit unsigned normalized RGB specular color
   vec3  specular;
   /// Specular exponent in the range `[0, 256]`

commit 538f7ee9cf85b376f50ee98456e8cf04e35ba6b9

Fix normal compression broken by 7f8866530f2869241f3708797bf211a78c2e888f
All normal vectors in structs were renamed to ensure that any shaders
still referring to old definitions will fail to compile. The normal
vector returned by shaders is now unconditionally compressed.

io7m-r2-shaders/src/main/glsl/com/io7m/r2/shaders/sources/R2SurfaceBasic.h
@@ -38,7 +38,7 @@ R2_deferredSurfaceMain (
   return R2_surface_output_t (
     surface.xyz,
     emission,
-    derived.normal_eye,
+    derived.normal_bumped,
     specular,
     R2_basic_surface_parameters.specular_exponent
   );

io7m-r2-shaders/src/main/glsl/com/io7m/r2/shaders/sources/R2SurfaceBasicReflective.h
@@ -69,7 +69,7 @@ R2_deferredSurfaceMain (
   return R2_surface_output_t (
     surface.xyz,
     emission,
-    derived.normal_eye,
+    derived.normal_bumped,
     specular,
     R2_basic_surface_parameters.specular_exponent
   );

io7m-r2-shaders/src/main/glsl/com/io7m/r2/shaders/sources/R2SurfaceShaderDriverBatched.h
@@ -24,26 +24,21 @@ layout(location = 2) out vec4 R2_out_specular;
 R2_gbuffer_output_t
 R2_surface_shader_main_gbuffer()
 {
-  vec3 normal = R2_normalsBump (
+  vec3 normal_bumped = R2_normalsBump (
     R2_surface_textures.normal,
     R2_surface_matrices_instance.transform_normal,
-    R2_vertex_data.normal,
+    R2_vertex_data.normal_vertex,
     R2_vertex_data.tangent,
     R2_vertex_data.bitangent,
     R2_vertex_data.uv
   );
-
-  vec2 normal_comp = R2_normalsCompress (normal);
 
   float depth_log = R2_logDepthEncodePartial (
     R2_vertex_data.positive_eye_z,
     R2_view.depth_coefficient);
 
   R2_surface_derived_t derived =
-    R2_surface_derived_t (
-      normal,
-      normal_comp
-    );
+    R2_surface_derived_t (normal_bumped);
 
   R2_surface_output_t o = R2_deferredSurfaceMain (
     R2_vertex_data,
@@ -56,7 +51,7 @@ R2_surface_shader_main_gbuffer()
   return R2_gbuffer_output_t(
     o.albedo,
     o.emission,
-    normal_comp,
+    R2_normalsCompress (o.normal),
     o.specular,
     o.specular_exp / 256.0,
     depth_log

io7m-r2-shaders/src/main/glsl/com/io7m/r2/shaders/sources/R2SurfaceShaderDriverSingle.h
@@ -22,26 +22,21 @@ layout(location = 2) out vec4 R2_out_specular;
 R2_gbuffer_output_t
 R2_surface_shader_main_gbuffer()
 {
-  vec3 normal = R2_normalsBump (
+  vec3 normal_bumped = R2_normalsBump (
     R2_surface_textures.normal,
     R2_surface_matrices_instance.transform_normal,
-    R2_vertex_data.normal,
+    R2_vertex_data.normal_vertex,
     R2_vertex_data.tangent,
     R2_vertex_data.bitangent,
     R2_vertex_data.uv
   );
-
-  vec2 normal_comp = R2_normalsCompress (normal);
 
   float depth_log = R2_logDepthEncodePartial (
     R2_vertex_data.positive_eye_z,
     R2_view.depth_coefficient);
 
   R2_surface_derived_t derived =
-    R2_surface_derived_t (
-      normal,
-      normal_comp
-    );
+    R2_surface_derived_t (normal_bumped);
 
   R2_surface_output_t o = R2_deferredSurfaceMain (
     R2_vertex_data,
@@ -54,7 +49,7 @@ R2_surface_shader_main_gbuffer()
   return R2_gbuffer_output_t(
     o.albedo,
     o.emission,
-    o.normal,
+    R2_normalsCompress (o.normal),
     o.specular,
     o.specular_exp / 256.0,
     depth_log

io7m-r2-shaders/src/main/glsl/com/io7m/r2/shaders/sources/R2SurfaceTypes.h
@@ -18,7 +18,7 @@ struct R2_vertex_data_t {
   /// Object-space UV
   vec2 uv;
   /// Object-space vertex normal
-  vec3 normal;
+  vec3 normal_vertex;
   /// Tangent vector
   vec3 tangent;
   /// Bitangent vector
@@ -29,9 +29,7 @@ struct R2_vertex_data_t {
 
 struct R2_surface_derived_t {
   /// Final uncompressed eye-space normal produced by bump/normal mapping
-  vec3 normal_eye;
-  /// Compressed eye-space normal
-  vec2 normal_compressed;
+  vec3 normal_bumped;
 };
 
 /// Textures that are required by all surfaces